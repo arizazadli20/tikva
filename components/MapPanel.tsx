1f64db36e92ae49350e75c3f7106cdc92bbda4fd
Ariz Azadli <arizazadli20@gmail.com>
2026-07-24 03:08:04 +0400

    fix: use cartodb for map tiles to resolve vercel 401

--- components/MapPanel.tsx
+++ components/MapPanel.tsx
@@ -113,8 +113,8 @@ export default function MapPanel({ port, detections }: Props) {
         zoomControl: true,
       });
       L.tileLayer(
-        "https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png",
-        { attribution: '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a> &copy; <a href="https://openstreetmap.org">OpenStreetMap</a>', maxZoom: 19 }
+        "https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png",
+        { attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>', maxZoom: 19 }
       ).addTo(map);
       mapInst.current = map;
       if (mounted) setLoaded(true);
